84102d84c792a211407422575ba05b5a663cf186
GustavoGesualdo <gustavohgesualdo@gmail.com>
2026-05-01 21:31:07 -0300

    Ajustes finais para static files no Vercel

--- django_basico/settings.py
+++ django_basico/settings.py
@@ -127,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')        # ← Adicione esta linha
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]  # mantenha se você usa assim
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Se você guarda os arquivos estáticos dentro de templates/static
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
