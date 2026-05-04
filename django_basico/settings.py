7b9b5ba514fd3f54f19632ad41c42be8c216ccfb
GustavoGesualdo <gustavohgesualdo@gmail.com>
2026-05-04 20:29:25 -0300

    Settings.py templates

--- django_basico/settings.py
+++ django_basico/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
 }
 
 # ================== STATIC FILES ==================
-STATIC_URL = '/static/'
+STATIC_URL = 'templates/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static'),
